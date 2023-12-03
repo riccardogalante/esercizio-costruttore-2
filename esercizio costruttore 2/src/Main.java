d98149a5192002acd8b509698a5f8f7d92efc80d
riccardogalante <56305199+riccardogalante@users.noreply.github.com>
2023-12-03 19:07:48 +0100

    Add files via upload

---
+++ esercizio costruttore 2/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Auto auto= new Auto(3000, "GP674VR", "lamborghini", "Veneno");
+        System.out.println(auto);
+
+    }
+}
